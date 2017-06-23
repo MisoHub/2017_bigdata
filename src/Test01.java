dc1567cccd9c8bec0efdbd6211f74a700999e30e
Paul_Kim <wantkps@gmail.com>
2017-06-23 17:59:01 +0900

    Revert "rever test 01"
    This reverts commit 8ffb3058d5de49901f7a3070fffe3e7e0b89e79c.

---
+++ src/Test01.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+/**
+ * Created by 78582 on 2017-06-23.
+ */
+public class Test01 {
+    public static Date getDate() {
+        Date d = new Date();
+        return d;
+    }
+
+    public static void main(String[] argvs) {
+        System.out.println("Hello Git World !");
+        System.out.println("** Date: " + getDate());
+    }
+}
+
+
+
