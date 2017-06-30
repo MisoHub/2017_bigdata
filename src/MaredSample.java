d554b28deac5c308236bdde1a5f75c889f5347f0
Paul_Kim <wantkps@gmail.com>
2017-06-30 17:33:27 +0900

    no message

---
+++ src/MaredSample.java
@@ -0,0 +1,7 @@
+/**
+ * Created by 78582 on 2017-06-30.
+ */
+public class MaredSample {
+
+
+}
--- src/WordCount.java
+++ src/WordCount.java
@@ -102,23 +102,42 @@ public class WordCount extends Configured implements Tool {
 
     pulbic int run(String[] args) throws Exception
     {
+
+        // 인자값이 제대로 들어왔는지 확인
         if(args.length <= 2)
         {
             System.err.printIn("Usage : wordcount <in> <out>");
             System.exit(2);
         }
 
-//      import org.apache.hadoop.mapreduce.Job;
+        /*
+          import org.apache.hadoop.mapreduce.Job;
+
+          Configuration 인스턴스와 잡이름"word count"로 Job을 생성함.
+          getConf 는 Job의 method???
+
+        */
+
         Job job = Job.getInstance(getConf(), "word count");
 
+        // Map 은 InputSplit 에 따라서 태스크 갯수가 설정되지만,
+        // Reduce는 Map결과 출력되는 key의 수를 고려하여 에 따라 사용자가 지정해줘야 한다.
         job.setNumReduceTasks(2);
+
+        // Main Class를 인수로 mapred.jar를 수행시킨다. 잡을 등록하면, mapred.jar 에 지정한 jar파일이 HDFS 상에 업로드 되고,
+        // 그것이 슬레이브 노드로 배포가 되어 수행되는 구조로 되어 있다. 일반적으로 main, mapper, reducer 각각 jar로 생성하여 설정한다.
         job.setJarByClass(WordCount.class);
+
+        // 각 단계에서 어떤 class를 사용할지 정의하는 것이며, 결과적으로 Configuration 인스턴스에 클래스와 관련된 속성값이 저장된다.
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
 
+        // map 의 output key, value 타입 설정
         job.setMapOutputKeyClasS(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
+
+        // reduce 의 output key, value 타입 설정
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
@@ -131,14 +150,25 @@ public class WordCount extends Configured implements Tool {
             DBOInputFormat/DBOOutputFormat, SequenceFileInputformat, SequenceFileOutputFormat 이 있다.
 
          */
+
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
+        // 입출력을 위한 디렉토리 지정
         TextInputFormat.addInputPath(job, new Path(args[0]));
         TextOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        // CDH4에서 Hadoop 1.x 버전의 MapReduce를 사용할 때 발생하는 문제로 생략하면, 에러메세지 발생하지만 동작에는 문제없다.
         job.getConfiguration().setBoolean("mapred.used.genericoptionsparser", true);
 
+
+        /*
+            boolean waitForCompletion(boolean verbos) : 위에서 설정한 작업을 submit 메소드로 등록하고
+            등록이 끝날 때까지 기다리는 작업이다. waitForComplete 수행 없이 submit을 직접 수행하게되면, 상태 완료를 확인 안하고
+             바로 리턴된다. 이 방법을 이용해서 복수의 잡을 한꺼번에 투입하는 코드를 만들 수 있다. 그리고.. 잡 등록중 강제종료(ctrl+c)
+             를 수행해도 등록된 작업은 취소되지 않는다. 잡을 투입하면서 프로그램 내에서 조건으로 잡을 중지하고 싶다면 killjob 메소드를 사용하면 된다.
+
+         */
         return (job.waitForCompletion(true) ? 0: 1);
     }
 
@@ -148,6 +178,12 @@ public class WordCount extends Configured implements Tool {
             import org.apache.hadoop.config.Configuration;
             import org.apache.hadoop.config.Configured;
             import org.apache.hadoop.tuil.ToolRunner;
+
+            ToolRunner : hadoop에서 사용되는 GenericOptionsParser를 이용하여 공통 옵션 인수를 해석하고
+            Configuration 인스턴스에 결과를 저장한 후 Tool.run 을 호출한다.
+
+            ToolRunner.run(Configuration, Tool, String[]) : Configuration 객체가 null이라면 그것을 생성하고,
+            Tool 객체와 인자값으로 Configuration 객채를 초기화 한다.
 
          */
         int res = ToolRunner.run(new Configuration(), new WordCount(), args);
